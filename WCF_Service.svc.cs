764ba2766bbf5e0b4893df1674cff76f1394eb28
tehila <tehila@carmel-tech.com>
2017-08-30 11:50:23 +0300

    WCF

---
+++ WCF_Service.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Service
+{
+    public class WCF_Service : IWCF_Service
+    {
+        
+        public  Int32 Add(Int32 num1,Int32 num2)
+        {
+            return num1 + num2;
+        }
+
+
+        public Int32 Subtract(Int32 num1, Int32 num2)
+        {
+            return num1 - num2;
+        }
+
+
+        public Int32 Multiply(Int32 num1, Int32 num2)
+        {
+            return num1 * num2;
+        }
+
+        public Int32 Divide(Int32 num1, Int32 num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
+# Wcf_Service

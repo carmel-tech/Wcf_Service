0c0522ef4cf4ca006d6b1cca1fc4703b6b9dc076
carmel-tech <31235634+carmel-tech@users.noreply.github.com>
2017-08-30 11:53:16 +0300

    Add files via upload

---
+++ IWCF_Service.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Service
+{
+   
+    [ServiceContract]
+    public interface IWCF_Service
+    {
+        [OperationContract]
+        Int32 Add(Int32 num1,Int32 num2);
+
+        [OperationContract]
+        Int32 Subtract(Int32 num1, Int32 num2);
+
+        [OperationContract]
+        Int32 Multiply(Int32 num1, Int32 num2);
+
+        [OperationContract]
+        Int32 Divide(Int32 num1, Int32 num2);
+    }
+}
